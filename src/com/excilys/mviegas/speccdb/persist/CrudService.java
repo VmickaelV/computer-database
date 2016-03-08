56bcd308460b2a35dedfb4af6e3d653c9c861aa2
VIEGAS Mickael <mviegas@excilys.com>
2016-03-08 17:50:44 +0100

    Ajout des classes métiers : Ortindateur et Entreprise. Ajout de leur DAO

---
+++ src/com/excilys/mviegas/speccdb/persist/CrudService.java
@@ -0,0 +1,30 @@
+package com.excilys.mviegas.speccdb.persist;
+
+import java.util.List;
+import java.util.Map;
+
+public interface CrudService<T> {
+    public T create(T t);
+
+    public T find(long id);
+
+    public T update(T t);
+
+    public boolean delete(long id);
+    
+    public boolean delete(T t);
+
+    public boolean refresh(T t);
+
+    List<T> findAll();
+    
+    List<T> findAll(int start, int size);
+
+    public List<T> findWithNamedQuery(String queryName);
+
+    public List<T> findWithNamedQuery(String queryName, int resultLimit);
+
+    public List<T> findWithNamedQuery(String namedQueryName, Map<String, Object> parameters);
+
+    public List<T> findWithNamedQuery(String namedQueryName, Map<String, Object> parameters, int resultLimit);
+}
