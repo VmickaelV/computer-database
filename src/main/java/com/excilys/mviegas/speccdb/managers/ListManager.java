d1a1f26e5b6764044ffe49a4623feec2afc740c6
VIEGAS Mickael <mviegas@excilys.com>
2016-03-14 16:44:50 +0100

    Correction pilote Mysql JDBC TomCat Dashboard semi dynamique redirection page arrivée

---
+++ src/main/java/com/excilys/mviegas/speccdb/managers/ListManager.java
@@ -0,0 +1,82 @@
+package com.excilys.mviegas.speccdb.managers;
+
+import java.util.List;
+
+import javax.annotation.ManagedBean;
+import javax.annotation.PostConstruct;
+
+import com.excilys.mviegas.speccdb.data.Computer;
+import com.excilys.mviegas.speccdb.persist.CrudService;
+import com.excilys.mviegas.speccdb.persist.jdbc.ComputerDao;
+
+@ManagedBean
+public class ListManager {
+	
+	public static final int DEFAULT_SIZE_PAGE = 10;
+
+	//===========================================================
+	// Attributes - privates
+	//===========================================================
+	
+	private String mFilter;
+	
+	private int mPage;
+	private int mSize;
+	
+	private List<Computer> mDisplayedComputers;
+	
+	private CrudService<Computer> mComputerDao;
+	
+	//===========================================================
+	// Constructeur
+	//===========================================================
+	public ListManager() {
+		init();
+	}
+	
+	//===========================================================
+	// Callback
+	//===========================================================
+	@PostConstruct
+	public void init() {
+		mFilter = "";
+		mPage = 0;
+		mSize = DEFAULT_SIZE_PAGE;
+		mComputerDao = ComputerDao.INSTANCE;
+		setPage(mPage);
+	}
+	
+	//===========================================================
+	// Méthodes controleurs
+	//===========================================================
+	
+	public int nbComputers() {
+		return mComputerDao.size();
+	}
+	
+	public int getPage() {
+		return mPage;
+	}
+
+	public void setPage(int pPage) {
+		mPage = pPage;
+		mDisplayedComputers = mComputerDao.findAll(mPage*mSize, mSize);
+	}
+
+	public String getFilter() {
+		return mFilter;
+	}
+
+	public void setFilter(String pFilter) {
+		mFilter = pFilter;
+		mDisplayedComputers = mComputerDao.findAll(mPage*mSize, mSize);
+	}
+
+	public List<Computer> getDisplayedComputers() {
+		return mDisplayedComputers;
+	}
+	
+	
+	
+	
+}
