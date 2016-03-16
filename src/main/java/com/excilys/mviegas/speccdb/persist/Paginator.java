577386265be04be2bfc4975fe3a02ce3cf38fb21
VIEGAS Mickael <mviegas@excilys.com>
2016-03-16 11:07:04 +0100

    Nettoyage de  dépendances dans pom.xml Ajout d'une pagination

---
+++ src/main/java/com/excilys/mviegas/speccdb/persist/Paginator.java
@@ -0,0 +1,17 @@
+package com.excilys.mviegas.speccdb.persist;
+
+/**
+ * Created by excilys on 15/03/16.
+ */
+public class Paginator<T> {
+
+	//===========================================================
+	// Attributes - private
+	//===========================================================
+	private int mCurrentPage;
+	
+	private int mElementsCount;
+	
+	private int mElementsByPage;
+
+}
