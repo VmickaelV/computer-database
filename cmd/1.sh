9ada4639bb430b150ab2eb09a6a19857b3bb6f33
VIEGAS Mickael <mviegas@excilys.com>
2016-03-31 16:40:52 +0200

    Préparation pour Docker et Jenkins

--- cmd/1.sh
+++ cmd/1.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+docker network create private
+
+docker run --privileged --name docker -d --net=private -v /var/lib/docker docker:dind
+
 docker build -t mviegas/jenkins -f dockers/Dockerfile_jenkins .
+
+docker run -p 8080:8080 -p 50000:50000 --net=private --rm --name jenkins mviegas/jenkins
+
