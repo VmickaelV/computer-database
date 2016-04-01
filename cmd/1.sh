1a8687779f5d0a11c224372ba6eddd8f781e4a40
VIEGAS Mickael <mviegas@excilys.com>
2016-04-01 09:37:17 +0200

    Préparation pour Docker et Jenkins

--- cmd/1.sh
+++ cmd/1.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+docker build -t bdd-mysql -f dockers/db/Dockerfile_mysql .
+docker run --name bdd -e MYSQL_ALLOW_EMPTY_PASSWORD=true -d --net=private bdd-mysql
+
+if [[ -n  $(docker ps -q -f status=running -f name=bdd) ]]; then
+	docker stop bdd
+fi
+
+if [[ -n  $(docker ps -q -f name=bdd) ]]; then
+	docker rm bdd
+fi
+
 docker network create private
 
 docker run --privileged --name docker -d --net=private -v /var/lib/docker docker:dind
