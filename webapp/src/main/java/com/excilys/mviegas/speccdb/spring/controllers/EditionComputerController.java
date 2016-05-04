907d9954f8530f2860f65d15d0f7543ce9cfa829
VIEGAS Mickael <viegas.mickael@gmail.com>
2016-05-04 09:10:20 +0200

    Localization
    Ajout localisation validation jquery

--- webapp/src/main/java/com/excilys/mviegas/speccdb/spring/controllers/EditionComputerController.java
+++ webapp/src/main/java/com/excilys/mviegas/speccdb/spring/controllers/EditionComputerController.java
@@ -7,6 +7,7 @@ import com.excilys.mviegas.speccdb.spring.singletons.ListOfCompanies;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,6 +40,7 @@ public class EditionComputerController {
 
 		pModelMap.put("computerEditor", pComputerEditorPage);
 		pModelMap.put("listOfCompanies", mListOfCompanies);
+		pModelMap.put("lang", LocaleContextHolder.getLocale());
 		return "addComputer";
 	}
 
@@ -54,6 +56,7 @@ public class EditionComputerController {
 		} else {
 			pModelMap.put("computerEditor", pComputerEditorPage);
 			pModelMap.put("listOfCompanies", mListOfCompanies);
+			pModelMap.put("lang", LocaleContextHolder.getLocale());
 			return "addComputer";
 		}
 	}
@@ -66,6 +69,7 @@ public class EditionComputerController {
 		
 		pModelMap.put("computerEditor", pComputerEditorPage);
 		pModelMap.put("listOfCompanies", mListOfCompanies);
+		pModelMap.put("lang", LocaleContextHolder.getLocale());
 		return "addComputer";
 	}
 
@@ -81,6 +85,7 @@ public class EditionComputerController {
 		} else {
 			pModelMap.put("computerEditor", pComputerEditorPage);
 			pModelMap.put("listOfCompanies", mListOfCompanies);
+			pModelMap.put("lang", LocaleContextHolder.getLocale());
 			return "addComputer";
 		}
 	}
