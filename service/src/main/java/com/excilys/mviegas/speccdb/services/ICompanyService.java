643a0f4c744d14d3a6244c0535dceb893b8a4406
VIEGAS Mickael <mviegas@excilys.com>
2016-05-12 11:29:44 +0200

    Refacto de Service

--- service/src/main/java/com/excilys/mviegas/speccdb/services/ICompanyService.java
+++ service/src/main/java/com/excilys/mviegas/speccdb/services/ICompanyService.java
@@ -4,6 +4,8 @@ import com.excilys.mviegas.speccdb.data.Company;
 import com.excilys.mviegas.speccdb.persistence.Crudable;
 
 /**
+ * Interface d'utilssation d'un CompanyService
+ *
  * Created by excilys on 22/04/16.
  */
 public interface ICompanyService extends Crudable<Company> {
--- service/src/main/java/com/excilys/mviegas/speccdb/services/IComputerService.java
+++ service/src/main/java/com/excilys/mviegas/speccdb/services/IComputerService.java
@@ -4,6 +4,8 @@ import com.excilys.mviegas.speccdb.data.Computer;
 import com.excilys.mviegas.speccdb.persistence.Crudable;
 
 /**
+ * Interface d'utilssation d'un ComputerService
+ *
  * Created by excilys on 22/04/16.
  */
 public interface IComputerService extends Crudable<Computer> {
