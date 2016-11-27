0a2edfaccbb861fd44e18abd39ab1859173ada7b
VIEGAS Mickael <viegas.mickael@gmail.com>
2016-11-27 18:13:36 +0100

    [FIX] Remove deprecated class in SwaggerInfo

--- swagger/src/main/java/com/excilys/mviegas/computer_database/configurations/SwaggerConfig.java
+++ swagger/src/main/java/com/excilys/mviegas/computer_database/configurations/SwaggerConfig.java
@@ -5,11 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 
 @Configuration
@@ -23,9 +24,8 @@ public class SwaggerConfig {
 				.paths(PathSelectors.any())
 				.build()
 				.apiInfo(apiInfo())
-//                .useDefaultResponseMessages(false)
-                .consumes(new HashSet<>(Arrays.asList("application/json")))
-                .produces(new HashSet<>(Arrays.asList("application/json")))
+                .consumes(new HashSet<>(Collections.singletonList("application/json")))
+                .produces(new HashSet<>(Collections.singletonList("application/json")))
 
 				;
 	}
@@ -35,10 +35,14 @@ public class SwaggerConfig {
 				"This is a REST Api for using the Computer Database",
 				"0.37",
 				null,
-				"VIEGAS Mickael",
+				contact(),
 				null,
 				null
 		);
+	}
+
+	private Contact contact() {
+		return new Contact("VIEGAS Mickael", "http://github.com/VmickaelV", "viegas.mickael@gmail.com");
 	}
 }
 
